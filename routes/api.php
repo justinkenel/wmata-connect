d0d0cc1c2703c187079f260e7524bbd821510150
Justin Kenel <jfkenel@gmail.com>
2022-04-10 19:27:04 -0400

    First API Call

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Http;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+
+Route::get('/station', function() {
+    $API_KEY = $_ENV['WMATA_API_KEY'];
+    $WMATA_API_URI = 'https://api.wmata.com/Rail.svc/json';
+    try {
+        $res = Http::withHeaders([
+            'api_key' => $API_KEY
+        ])->get("$WMATA_API_URI/jStations");
+        return $res->body();
+    } catch(Exception $e) {
+        return $e->getMessage();
+    }
 });
